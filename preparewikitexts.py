65ef854ec7b2477432364fe872f01382c13a7041
Tigran Mardanyan <mrde00@gmail.com>
2018-09-19 08:30:31 +0400

    prepare corrected

--- preparewikitexts.py
+++ preparewikitexts.py
@@ -1,1 +1,38 @@
-sadf
+
+
+from __future__ import print_function
+import numpy as np
+import theano
+import codecs
+import json
+import argparse
+import utils
+from datetime import datetime
+
+import shutil
+import glob
+
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--sourcepath', default='/home/tigran/rau/translit/files/text/')
+	parser.add_argument('--destpath', default='/home/tigran/rau/translit/translit/languages/hy-AM/data/all.txt')
+	args = parser.parse_args()
+
+	outfilename = args.destpath
+
+	with open(outfilename, 'wb') as outfile:
+		for filename in glob.glob('wiki__*'):
+			if filename == outfilename:
+			# don't want to copy the output into the output
+				continue
+			print(filename)
+		#with open(filename, 'rb') as readfile:
+		#	shutil.copyfileobj(readfile, outfile)
+
+
+
+
+
+
+if __name__ == '__main__':
+	main()
